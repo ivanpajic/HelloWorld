26b98d7f3412c3826eff2bbb7664f91f3ab884b8
Ivan.Pajic <ivan.pajic@jetbrains.com>
2023-02-08 09:58:10 +0100

    Add Branch1 method

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,4 @@
+import javax.management.InstanceAlreadyExistsException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,12 +7,28 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
-        int[] is = {1,2,3};
+        int[] is = {1, 2, 3};
         for (int i : is) {
-            System.out.println(i/0);
+            System.out.println(i + 1 / i);
         }
+        System.out.println("Run ended");
+
+        String myString = "blah "
+                + "blah2 "
+                + "blah3";
+
+        boolean b = false;
+        Instance instance = new Instance();
+
 
     }
 
+    public void testMain() {
+        System.out.println("Test me!");
+    }
 
+    public String branch1Method() {
+        String text = "This is some text";
+        return text;
+    }
 }
