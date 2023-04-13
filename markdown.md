9a612d266e96e048c4571e48860b7f5676df4843
Ivan Pajic <ivan.pajic@jetbrains.com>
2023-04-13 12:03:13 +0200

    add rest of the files to level the repository

--- markdown.md
+++ markdown.md
@@ -1,3 +1,10 @@
 ``test `backtick` string``
 
 split view
+
+
+| Development | Launches | Audio | A Notes | Last |
+|-------------|----------|-------|---------|------|
+| Optimizing  | ✅        |       |         |      |
+|             | ⚠️       |       |         |      |
+|             |          |       |         |      |
--- src/main/java/Entity.java
+++ src/main/java/Entity.java
@@ -1,8 +1,84 @@
+import java.util.*;
+import java.util.logging.Logger;
+
 public class Entity {
 
     String entityName;
 
     public Entity() {
         this.entityName = "Entity1";
+    }
+
+    public CombineLeftover testMethod() {
+        Map<List, String> leftoverTitles = new Map<List, String>() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean containsKey(Object key) {
+                return false;
+            }
+
+            @Override
+            public boolean containsValue(Object value) {
+                return false;
+            }
+
+            @Override
+            public String get(Object key) {
+                return null;
+            }
+
+            @Override
+            public String put(List key, String value) {
+                return null;
+            }
+
+            @Override
+            public String remove(Object key) {
+                return null;
+            }
+
+            @Override
+            public void putAll(Map<? extends List, ? extends String> m) {
+
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public Set<List> keySet() {
+                return null;
+            }
+
+            @Override
+            public Collection<String> values() {
+                return null;
+            }
+
+            @Override
+            public Set<Entry<List, String>> entrySet() {
+                return null;
+            }
+        };
+        List<ProcessBuilder.Redirect> redirectWorks = new ArrayList<>();
+        Logger combineLog = null;
+        return new CombineLeftover(leftoverTitles, redirectWorks, combineLog);
+    }
+
+    public record CombineLeftover(Map<List, String> titlesWithStillWrongWork,
+                                  List<ProcessBuilder.Redirect> redirectWorks,
+                                  Logger combineLog) {
+
     }
 }
