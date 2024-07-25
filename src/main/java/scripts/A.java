5c7d0bff2976d4dc4dd0c53fdd0146f470b3b770
Ivan Pajic <ivan.pajic@jetbrains.com>
2024-07-25 11:51:12 +0200

    Modify couple of files

--- src/main/java/scripts/A.java
+++ src/main/java/scripts/A.java
@@ -1,2 +1,16 @@
-package scripts;public class A {
+package scripts;
+
+public class A {
+    void foo(int value) {
+        class Local {
+            void bar() {
+                Runnable runnable = () -> {
+                    getPrintln();// extract method on this statement
+                };
+            }
+
+            private static void getPrintln() {
+            }
+        }
+    }
 }
