d21d6f9e1c14055b7c106990f0a960edbc4494e8
Ivan Pajic <ivan.pajic@jetbrains.com>
2024-12-18 16:35:26 +0100

    ADd Scratch.java

---
+++ src/main/java/scripts/Scratch.java
@@ -0,0 +1,14 @@
+package scripts;
+
+class Scratch {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(2)
+
+        class MyClass1 {}
+        class MyClass2 {}
+
+        int x = 0;
+    }
+}
