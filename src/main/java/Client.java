9220065c391c4cb9478491c1e43ad3627d97204c
Ivan Pajic <ivan.pajic@jetbrains.com>
2025-02-13 12:04:15 +0100

    Commit and push to 123-feature

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -11,7 +11,7 @@ public class Client {
 
         return identification;
     }
-
+// change in client
     public void setIdentification(String identification) {
         this.identification = identification;
     }
