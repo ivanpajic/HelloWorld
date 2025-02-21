508974de18a3b859a8776b7cc45724aa0d285aa6
Ivan Pajic <ivan.pajic@jetbrains.com>
2025-02-21 14:27:46 +0100

    Commit 3 rename

--- src/main/java/TestRenameGit3.java
+++ src/main/java/TestRenameGit3.java
@@ -1,11 +1,11 @@
-public class TestRenameGit2 {
+public class TestRenameGit3 {
 
     public static void main(String[] args) {
         EmployeeNew employee = new EmployeeNew("John Doe", 25.0);
         employee.setHoursWorked(40);
-        System.out.println("Dino2: " + employee.calculateSalary());
+        System.out.println("Dino3: " + employee.calculateSalary());
         System.out.println("Ivan: " + employee.calculateSalary());
-        System.out.println("Salary ivan2: " + employee.calculateSalary());
+        System.out.println("Salary ivan3: " + employee.calculateSalary());
     }
 
 
