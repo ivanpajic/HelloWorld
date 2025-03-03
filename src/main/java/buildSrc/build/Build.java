76e84174c512514886b8e476c6989cf2bb9f4fe4
Ivan Pajic <ivan.pajic@jetbrains.com>
2025-03-03 15:38:15 +0100

    change in Build.java file

--- src/main/java/buildSrc/build/Build.java
+++ src/main/java/buildSrc/build/Build.java
@@ -11,7 +11,7 @@ public class Build {
         this.buildVersion = buildVersion;
 
     }
-
+// change in DEF branch
     public Build(String buildVersion) {
         this.buildVersion = buildVersion;
     }
