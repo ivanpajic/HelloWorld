2e71cc9473b2262adc1146a1bb451cc133fab5b2
Ivan Pajic <ivan.pajic@jetbrains.com>
2025-04-14 15:07:12 +0200

    Update main print statement in Main.java
    Revised the primary output message for clarity and emphasis. This change aims to improve consistency and better reflect the intended functionality.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Here is another change");
+        System.out.println("No this is the main change!");
         System.out.println("Runing single classes!");
     }
 }
