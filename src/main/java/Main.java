e7db7e3f345831fda05fa19505548817d883aa3a
Ivan Pajic <ivan.pajic@jetbrains.com>
2025-04-14 15:06:27 +0200

    Update log message in main method
    Revised the printed output in the main method to reflect a new message. Removed an outdated log and fixed a typo in the previous message. This ensures clarity and resolves potential conflicts.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Here is another change");
-        System.out.println("Runing single classes!");
+        System.out.println("New change with conflict");
     }
 }
