db4305c1c15847cc8b26d78bbe53d5d2439e25f6
Ivan Pajic <123973602+ivanpajic@users.noreply.github.com>
2025-05-09 10:44:44 +0200

    Update Solution.java

--- src/main/java/server/SolutionRenamed.java
+++ src/main/java/server/SolutionRenamed.java
@@ -10,7 +10,7 @@ public class SolutionRenamed {
         int a = Integer.parseInt(reader.readLine());
 // change
         if (a > 0) {
-            a = a * 1;
+            a = a * 1 - 20;
         } else if (a < 0) {
             a = a + 1;
         }
