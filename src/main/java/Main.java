f8ddb1f8ca301a1ae73bb5b8206aae09e8ab4eb2
Ivan Pajic <ivan.pajic@jetbrains.com>
2025-05-13 15:46:27 +0200

    deleted change in Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("No this is the main change!");
         System.out.println("Runing single classes!");
-        // change
     }
 }
