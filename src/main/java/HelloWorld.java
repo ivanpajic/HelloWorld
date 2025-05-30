25b2e6f1880142309584f31610f0e63c154f06a2
Ivan Pajic <ivan.pajic@jetbrains.com>
2025-05-30 14:06:23 +0200

    conflicting change but in Main

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -33,7 +33,7 @@ public class HelloWorld {
         String teststr = "40|50|60";
         boolean b = false;
         for(i=0; i++; i<10) {
-
+            // This is the change
         }
 //add a change
         HelloWorld hello = new HelloWorld();
