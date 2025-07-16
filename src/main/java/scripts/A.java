45f91cab47a29c7f5e5ffd5c81f190d0f7ce242c
Ivan Pajic <ivan.pajic@jetbrains.com>
2025-07-16 10:43:13 +0200

    new commit on betabranch
    Signed-off-by: Ivan Pajic <ivan.pajic@jetbrains.com>

--- src/main/java/scripts/A.java
+++ src/main/java/scripts/A.java
@@ -12,7 +12,7 @@ public class A {
             private static void getPrintln() {
                 // line added on the BetaBranch
             }
-            // line added in the IntroduceChange line change
+            // line added in
         }
     }
 }
