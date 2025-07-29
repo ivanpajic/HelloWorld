5f40cdfda3b2cba183a96902606fbe0b3e81ed3b
Ivan Pajic <123973602+ivanpajic@users.noreply.github.com>
2025-07-29 10:53:47 +0200

    Update EmployeeRenamed.java

--- src/main/java/EmployeeRenamed.java
+++ src/main/java/EmployeeRenamed.java
@@ -7,7 +7,7 @@ public class Employee {
         this.name = name;
         this.hourlyRate = hourlyRate;
     }
-// change on remote 12
+// change from remote repo
     public void setHoursWorked(int hoursWorked) {
         this.hoursWorked = hoursWorked;
     }
