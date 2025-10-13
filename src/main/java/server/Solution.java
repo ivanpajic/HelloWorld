0162134bacf9e82540103bcc6b77d47451e0f0ca
ivan pajic <ivan.pajic@jetbrains.com>
2025-10-13 10:37:57 +0200

    change for pull request

--- src/main/java/server/Solution.java
+++ src/main/java/server/Solution.java
@@ -8,7 +8,7 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(reader.readLine());
-
+// change for pull request
         if (a > 0) {
             a = a * 2;
         } else if (a < 0) {
