e85d632b745ce987a9bfcd839c586b3b9dbd5cdb
Ivan Pajic <ivan.pajic@jetbrains.com>
2025-10-17 10:58:59 +0200

    commit on branch for a pull request

--- src/main/java/buildSrc/src/Source.java
+++ src/main/java/buildSrc/src/Source.java
@@ -1,10 +1,11 @@
 package buildSrc.src;
 
 public class Source {
-
+    // change for first review
 	String source;
 
 	public Source() {
 		this.source = "source";
 	}
 }
+// change for second review
